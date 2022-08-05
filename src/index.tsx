ffd89f7dbae861acc9a7984fe49654eab8466a85
Ricardo Gava <ricardoagava@gmail.com>
2022-08-05 06:45:06 -0300

    Removed list style from all list elements

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   text-decoration: none;
+  list-style: none;
   box-sizing: border-box;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',

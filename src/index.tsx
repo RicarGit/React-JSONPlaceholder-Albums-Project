af7b23e5b06fb68124fab4eb2779c355ff13fc1d
Ricardo Gava <ricardoagava@gmail.com>
2022-08-05 06:14:28 -0300

    Reseting more elements on GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,11 @@ import { createGlobalStyle } from 'styled-components'
 import App from './App'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
   margin: 0;
+  padding: 0;
+  text-decoration: none;
+  box-sizing: border-box;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;

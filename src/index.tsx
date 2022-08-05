0917f21a210ee7b13c25b012863eb476ba40a483
Ricardo Gava <ricardoagava@gmail.com>
2022-08-04 23:49:02 -0300

    Removed index.css and moved all css to index.tsx in a GlobalStyle component from styled component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDom from 'react-dom/client'
 
-import './index.css'
+import { createGlobalStyle } from 'styled-components'
 import App from './App'
 
-const root = createRoot(
+const GlobalStyle = createGlobalStyle`
+  body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source - code - pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`
+
+const root = ReactDom.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>
 )

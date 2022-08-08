30dde39151884dc8b7b23451f4d341c22b2b3dab
Ricardo Gava <ricardoagava@gmail.com>
2022-08-07 23:59:41 -0300

    Adding the first Route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+
 import { Header } from "./components/Header"
 import { AlbumList } from "./components/AlbumList/"
+import { Footer } from "./components/Footer"
 
 function App() {
   return (
     <>
       <Header />
-      <AlbumList />
+      <Routes>
+        <Route path="/" element={<AlbumList />} />
+      </Routes>
+      <Footer />
     </>
   )
 }

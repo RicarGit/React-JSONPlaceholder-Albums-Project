bbb62e63dc43ffe4cebda5b8f29a0391f8225e07
Ricardo Gava <ricardoagava@gmail.com>
2022-08-07 23:58:24 -0300

    Changed Header and Footer styles to make a pattern between them

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,10 +2,11 @@ import styled from "styled-components"
 
 export const FooterContainer = styled.footer`
   height: 100px;
-  width: 80%;
+  width: 80vw;
   margin: 40px auto 0 auto;
-  padding: 8px;
+  padding: 8px 0 0 20px;
   font-size: 22px;
   font-weight: bold;
+  color: orangered;
   border-top: 3px solid orangered;
 `
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,11 +2,12 @@ import styled from "styled-components/macro"
 
 export const Container = styled.header`
   margin: 0 auto 30px auto;
+  padding-left: 20px;
   height: 100px;
   width: 80vw;
   display: flex;
   align-items: center;
-  border-bottom: 2px solid #ccc;
+  border-bottom: 3px solid #ccc;
   border-color: orangered;
   color: orangered;
 `

bf864d8e887aae9504abe5de6a08afdc79b717e1
Ricardo Gava <ricardoagava@gmail.com>
2022-08-10 02:59:23 -0300

    Updating photo component to display the bigger image based on the selected photo by his id

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import * as S from './styles'
 
-const photoUrl = 'a'
+import axios from 'axios'
+const axiosInstance = axios.create({ baseURL: `https://jsonplaceholder.typicode.com` })
 
 export const Photo = () => {
+  const [photoUrl, setPhotoUrl] = useState('')
+  const { photoID } = useParams()
+
+  useEffect(() => {
+    const getPhotoUrl = async () => {
+      const response = await axiosInstance.get(`/photos/${photoID}`)
+
+      if (response.data) {
+        setPhotoUrl(response.data.url)
+      }
+    }
+
+    getPhotoUrl()
+  }, [photoID])
+
   return (
     <S.PhotoBigContainer>
-      {photoUrl ? <S.PhotoImage src={photoUrl} /> : "A Foto Não Existe."}
+      {photoUrl ? <S.PhotoImage src={photoUrl} alt={'photo_600x600'} /> : "A Foto Não Existe."}
     </S.PhotoBigContainer>
   )
 }

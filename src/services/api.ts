278d3810c88e38b2f2f5e34c3a433b783f79bfbe
Ricardo Gava <ricardoagava@gmail.com>
2022-08-15 17:16:24 -0300

    Added another fetch to the api to get the albums title

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,12 @@ export const api = {
     const response = await axiosInstance(`/albums`)
     return response.data
   },
+
+  async getAlbumTitle(albumID: string) {
+    const response = await axiosInstance(`/albums/${albumID}`)
+    return response.data.title
+  },
+
   async getPhotos(albumID: string) {
     const response = await axiosInstance(`albums/${albumID}/photos`)
     return response.data

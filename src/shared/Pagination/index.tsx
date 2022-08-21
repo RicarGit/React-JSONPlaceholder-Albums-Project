2fdfaf543cdd5d0e7de93b63e632e6699c1551fd
Ricardo Gava <ricardoagava@gmail.com>
2022-08-21 00:01:00 -0300

    Refactoring: removed unused code in Pagination component

--- src/shared/Pagination/index.tsx
+++ src/shared/Pagination/index.tsx
@@ -1,4 +1,4 @@
-import { useState, SetStateAction } from 'react'
+import { SetStateAction } from 'react'
 import * as S from './styles'
 
 type Pages = {
@@ -10,18 +10,17 @@ type Pages = {
 export const Pagination = ({ pages, currentPage, setCurrentPage }: Pages) => {
   return (
     <S.Container>
-      <>
-        {Array
-          .from({ length: pages })
-          .map((_, index) =>
-            <S.PageButton
-              key={index}
+      {Array
+        .from({ length: pages })
+        .map((_, index) =>
+          <S.PageButton
+            key={index}
             className={currentPage === index ? 'active' : ''}
-            >
-              {index + 1}
-            </S.PageButton>
-          )}
-      </>
+            onClick={() => setCurrentPage(index)}
+          >
+            {index + 1}
+          </S.PageButton>
+        )}
     </S.Container>
   )
 }
